604702eadfb8e7a3ec3738d6caf650f2432550b2
Prince Ishaku <princeishaku50@gmail.com>
2026-07-20 06:43:39 -0400

    Fix deployment TypeScript errors

---
+++ src/types/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  id: number;
+  title: string;
+  category: string;
+  description: string;
+  technologies: string[];
+  image: string;
+  github: string;
+  demo: string;
+}
